72dfd522502543bc81fff27bf713832e0cfb7548
Gtp87 <fabio.giannitrapani@gmail.com>
2022-01-19 11:42:27 +0100

    add components

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<Search />
+</div>
+</template>
+
+<script>
+// import axios from 'axios';
+import Search from './Search.vue'
+export default {
+    name: 'Main',
+    components: {
+        Search,
+    }
+}
+</script>
+
+<style>
+
+</style>
